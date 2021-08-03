1713f99568af7915e048462b06c3f892361de1fd
Shashank Vemuri <49424748+Shashankv3@users.noreply.github.com>
2021-08-03 13:00:39 -0400

    Update email_stock_alert.py

--- Find_Stocks/email_stock_alert.py
+++ Find_Stocks/email_stock_alert.py
@@ -1,8 +1,6 @@
 import os
 import smtplib
-import imghdr
 from email.message import EmailMessage
-
 import yfinance as yf
 import datetime as dt
 import pandas as pd
@@ -11,31 +9,28 @@ import time
 
 EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
-
 msg = EmailMessage()
 
 yf.pdr_override()
-start =dt.datetime(2018,12,1)
-now = dt.datetime.now()
+start = dt.date.today() - dt.timedelta(days = int(365))
+end = dt.date.today()
 
-stock="QQQ"
-TargetPrice = 180
+stock = "TSLA"
+TargetPrice = 900
 
-msg['Subject'] = 'Alert on '+ stock+'!'
+msg['Subject'] = f'Alert on {stock}!'
 msg['From'] = ''
 msg['To'] = ''
 
 alerted=False
 
-while 1:
-
-	df = pdr.get_data_yahoo(stock, start, now)
+while True:
+	df = pdr.get_data_yahoo(stock, start, end)
 	currentClose=df["Adj Close"][-1]
 
-	condition=currentClose>TargetPrice
+	condition=currentClose > TargetPrice
 
 	if(condition and alerted==False):
-
 		alerted=True
 
 		message=stock +" Has activated the alert price of "+ str(TargetPrice) +\
@@ -45,7 +40,6 @@ while 1:
 		msg.set_content(message)
 
 		files=[r""]
-
 		for file in files:
 			with open(file,'rb') as f:
 				file_data=f.read()
@@ -54,12 +48,12 @@ while 1:
 				msg.add_attachment(file_data, maintype="application",
 					subtype='ocetet-stream', filename=file_name)
 
-
 		with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
 		    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
 		    smtp.send_message(msg)
-
 		    print("completed")
+			
 	else:
 		print("No new alerts")
+	
 	time.sleep(60)
